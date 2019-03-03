7cbf94d8bf3241b3dce883eb602c6eb74ec630ab
Derek Chan <34112635+YChan3@users.noreply.github.com>
2019-03-03 00:02:36 -0500

    1

---
+++ myParser.py
@@ -0,0 +1,73 @@
+from display import *
+from matrix import *
+from draw import *
+
+"""
+Goes through the file named filename and performs all of the actions listed in that file.
+The file follows the following format:
+     Every command is a single character that takes up a line
+     Any command that requires arguments must have those arguments in the second line.
+     The commands are as follows:
+         line: add a line to the edge matrix -
+               takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
+         ident: set the transform matrix to the identity matrix -
+         scale: create a scale matrix,
+                then multiply the transform matrix by the scale matrix -
+                takes 3 arguments (sx, sy, sz)
+         translate: create a translation matrix,
+                    then multiply the transform matrix by the translation matrix -
+                    takes 3 arguments (tx, ty, tz)
+         rotate: create a rotation matrix,
+                 then multiply the transform matrix by the rotation matrix -
+                 takes 2 arguments (axis, theta) axis should be x y or z
+         apply: apply the current transformation matrix to the edge matrix
+         display: clear the screen, then
+                  draw the lines of the edge matrix to the screen
+                  display the screen
+         save: clear the screen, then
+               draw the lines of the edge matrix to the screen
+               save the screen to a file -
+               takes 1 argument (file name)
+         quit: end parsing
+
+See the file script for an example of the file format
+"""
+def parse_file( fname, points, transform, screen, color ):
+    file = []
+    with open(fname, 'r') as f:
+        line = f.readline().strip()
+        while line != "":
+            print(line)
+            if line == "line":
+                args = f.readline().split()
+                add_edge(points, int(args[0]), int(args[1]), int(args[2]), int(args[3]), int(args[4]), int(args[5]))
+            elif line == "ident":
+                ident(transform)
+            elif line == "scale":
+                args = f.readline().split()
+                matrix_mult(make_scale(int(args[0]),int(args[1]),int(args[2])), transform)
+            elif line == "move":
+                args = f.readline().split()
+                matrix_mult(make_translate(int(args[0]),int(args[1]),int(args[2])), transform)
+            elif line == "rotate":
+                args = f.readline().split()
+                if args[0] == "x":
+                    matrix_mult(make_rotX(int(args[1])),transform)
+                elif args[0] == "y":
+                    matrix_mult(make_rotY(int(args[1])),transform)
+                elif args[0] == "z":
+                    matrix_mult(make_rotZ(int(args[1])),transform)
+            elif line == "apply":
+                matrix_mult(transform, points)
+            elif line == "display":
+                clear_screen(screen)
+                draw_lines(points, screen, color)
+                display(screen)
+            elif line == "save":
+                clear_screen(screen)
+                draw_lines(points, screen, color)
+                args = f.readline()
+                save_extension(screen, args)
+            elif line == "quit":
+                break
+            line = f.readline().strip()
